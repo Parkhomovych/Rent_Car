13158af8a1108e1a4823a586debee183f11f9be1
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2024-02-23 22:39:42 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ export const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/Rent_Car" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="catalog" element={<Catalog />} />
         <Route path="favorites" element={<Favorites />} />
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
